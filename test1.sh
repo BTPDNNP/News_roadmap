dc5a511d8caff9545d901b56a354d8032a1d6825
nitishsharma <sharma.nitish2010@gmail.com>
2015-01-16 10:20:34 +0530

    test commit

---
+++ test1.sh
@@ -0,0 +1,11 @@
+cd /home/nitin/workspace/news_finance/src/
+mkdir /home/nitin/Desktop/hogaya
+sudo javac Headlines.java
+mkdir /home/nitin/Desktop/hogayaok
+java Headlines
+mkdir /home/nitin/Desktop/hogaya1
+cd /home/nitin/workspace/newsarticle/src/
+sudo javac Newsandimages.java
+mkdir /home/nitin/Desktop/hogayaok2
+java Newsandimages
+mkdir /home/nitin/Desktop/hogaya2
